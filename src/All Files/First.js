f1b0627c8f5648a2b9bf6adefa3eb3760f733cd3
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-04 10:23:59 +0530

    edit file

--- src/All Files/First.js
+++ src/All Files/First.js
@@ -91,6 +91,20 @@ function First() {
           </div>
         </div>
       </div>
+
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div>
+              
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-4"></div>
+          <div className="col-sm-12 col-md-6 col-lg-4"></div>
+        </div>
+      </div>
+      
     </>
   )
 }
