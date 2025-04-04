b14d0df60bc52fd8dc3842cd1089613e245b1dc4
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-04 10:24:59 +0530

    add code

--- src/All Files/First.js
+++ src/All Files/First.js
@@ -105,7 +105,16 @@ function First() {
               </div>
             </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-4"></div>
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div>
+              <div className="card" style={{ "width": "18rem" }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="col-sm-12 col-md-6 col-lg-4"></div>
         </div>
       </div>
