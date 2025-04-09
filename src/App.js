058b1e12748f286eac931df364c52bfd86874c51
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-09 12:24:30 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Nav from './All Files/Nav';
 import First from './All Files/First';
+import Projects from './All Files/Projects';
+import Projects from './All Files/Projects';
 
 function App() {
   
   return (
   <>
+    <Projects/>
     <Nav/>
     <First/>
   </>
